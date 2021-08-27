0d22463e9a51f4dccb2feaba179a67d417c6fc26
Uchenna Ezeobi <uchenna.ezeobi3@gmail.com>
2021-08-27 11:21:05 -0600

    changed data path

--- reverse.py
+++ reverse.py
@@ -3,7 +3,7 @@ from schedule_attack.reverse_engineer.reverse_log import ReverseLogs
 def main():
 
     #s = ReverseLogs("data/20210826-173253_0-0.6233_log.csv", 500)
-    s = ReverseLogs('./data/OAK-old/Attack_free_dataset.csv', 500000)
+    s = ReverseLogs('./OAK-old/Attack_free_dataset.csv', 500000)
 
     s.period_bounds()
 
