5c9714a74ee4ac8437a50c4895fcb2e3f260d2b0
Uchenna Ezeobi <uchenna.ezeobi3@gmail.com>
2021-09-22 16:40:39 -0600

    Added logger file and parser

--- number_of_preceded_msg.py
+++ number_of_preceded_msg.py
@@ -1,56 +1,59 @@
 from schedule_attack.utils.process_file import *
 from schedule_attack.reverse_engineer.reverse_log import ReverseLogs
 from schedule_attack.utils.general import *
+from schedule_attack.utils.logger import CustomLogger
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data", type=str, help="CSV file to reverse engineer", default='./OAK-old/Attack_free_dataset.csv')
+parser.add_argument("--logger_name", type=str, help="Logger name to log data", default="preceded_logger.log")
+parser.add_argument("--bus_speed", type=int, help="Bus speed", default=500000)
+parser.add_argument("--num_period", type=int, help="Number of Hyperperiod to Evaluate", default=1)
+
+args = parser.parse_args()
+
+logger = CustomLogger(__name__,args.logger_name)
 
 
 
-def get_hyperperiod_list(data,hyperperiod=None):
-  hyperperiod_list = []
-  for msg in data:
-    if hyperperiod != None:
-      if msg.timestamp > hyperperiod:
-        return hyperperiod_list
-    hyperperiod_list.append(msg.id)
-  return hyperperiod_list
+def main():
 
-def get_preceded_id(sequence_list):
-    res = {}
-    for i in range(1, len(sequence_list)):
-        id = sequence_list[i]
-        if id in res:
-            if sequence_list[i-1] not in res[id]:
-                res[id].append(sequence_list[i-1])
+    r = ReverseLogs(args.data, args.bus_speed) #Using bus_speed of 500kbs
+    r_info = r.period_bounds()
+    data = ProcessFile.process_oak_old_data(args.data, args.bus_speed)
+    keys = [key for key in r_info if key != 1]
+    periods = [r_info[key] for key in keys if key != 1]
+
+    hyperperiod = General.lcm(periods)
+    logger.debug("Hyperperiod is: {} ".format(hyperperiod)) # Round off error from floating number
+    # Using manual inspection
+    hyperperiod = 1.009745
+    #hyperperiod = 1
+    sequence = args.num_period * hyperperiod
+
+    sequence_list = General.get_hyperperiod_list(data, sequence)
+
+    logger.debug("The sequence list are {}".format(sequence_list))
+
+    result = General.get_preceded_id(sequence_list)
+
+    num_res = {}
+
+    for key in result:
+        count = len(result[key])
+        if count in num_res:
+            num_res[count] += 1
         else:
-            res[id] = [sequence_list[i-1]]
+            num_res[count] = 1
 
-    return res
+    logger.debug("With {} number of hyperpeiod, the results are {}".format(args.num_period,num_res))
+    sum = 0
+    for key in num_res:
+        sum += num_res[key]
+        logger.debug("{} message id has {} number of preceded ID".format(num_res[key],key))
+    logger.debug("There is {} total number of messages".format(sum))
 
-file_path = './OAK-old/Attack_free_dataset.csv'
-bus_speed = 500000
-r = ReverseLogs(file_path, bus_speed) #Using bus_speed of 500kbs
-r_info = r.period_bounds()
-data = ProcessFile.process_oak_old_data(file_path, bus_speed)
-keys = [key for key in r_info if key != 1]
-periods = [r_info[key] for key in keys if key != 1]
+if __name__ == "__main__":
 
-hyperperiod = General.lcm(periods)
-print(hyperperiod) # Round off error from floating number
-# Using manual inspection
-hyperperiod = 1.009745
-#hyperperiod = 1
-sequence = 1 * hyperperiod
-
-sequence_list = get_hyperperiod_list(data, sequence)
-
-result = get_preceded_id(sequence_list)
-
-num_res = {}
-
-for key in result:
-    count = len(result[key])
-    if count in num_res:
-        num_res[count] += 1
-    else:
-        num_res[count] = 1
-
-print(num_res)
+    main()
