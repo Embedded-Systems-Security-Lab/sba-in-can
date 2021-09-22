c73fb345fd65a7bb694c1da68113b38e1c8bfa46
Uchenna Ezeobi <uchenna.ezeobi3@gmail.com>
2021-09-22 15:13:23 -0600

    added an argument parser

--- reverse.py
+++ reverse.py
@@ -1,13 +1,16 @@
 from schedule_attack.reverse_engineer.reverse_log import ReverseLogs
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data", type=str, help="CSV file to reverse engineer", default='./OAK-old/Attack_free_dataset.csv')
+parser.add_argument("--bus_speed", type=int, help="Number of distinct PIDs to simulate", default=500000)
+args = parser.parse_args()
 
 def main():
 
-    #s = ReverseLogs("data/20210826-173253_0-0.6233_log.csv", 500)
-    s = ReverseLogs('./OAK-old/Attack_free_dataset.csv', 500000)
-
+    s = ReverseLogs(args.data, args.bus_speed)
     s.period_bounds()
 
 if __name__ == "__main__":
-
     main()
 
